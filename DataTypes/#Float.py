016eddb1849a99c772f75d5f455e5c0a4d25aa18
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-01-21 14:33:24 +0530

    your message

---
+++ DataTypes/#Float.py
@@ -0,0 +1,25 @@
+#Float 
+a = 9.8 
+print(type(a))
+#complex 
+c = 3+4j
+print(type(c))
+print(c.real)
+print(c.imag)
+
+#Type Conversion 
+# Int to float 
+a = 10
+b = float(a)
+print(type(b))
+
+#Float to Int 
+a = 5.6
+b = int(a)
+print(type(b)) 
+
+#int to Complex 
+a = 10 
+b = complex(a)
+print(b)
+print(type(b))
