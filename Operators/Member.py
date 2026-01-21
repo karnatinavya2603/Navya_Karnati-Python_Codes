1650e776f5ed970516bad232f60cc256a28abe26
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-01-21 23:15:12 +0530

    Add membership operator code

---
+++ Operators/Member.py
@@ -0,0 +1,30 @@
+numbers = [1, 3, 5, 7, 9]
+print(5 in numbers)
+
+nums = [2, 4, 6, 8]
+print(10 not in nums)
+
+word = "banana"
+print('a' in word)
+
+sentence = "I am learning python"
+print("python" in sentence)
+
+lst = [10, 20, 30, 40, 50]
+n = int(input("Enter a number: "))
+print(n in lst)
+
+t = (10, 20, 25, 30)
+print(25 in t)
+
+s = {"apple", "banana", "cherry"}
+print("apple" in s)
+
+d = {"name": "Navya", "age": 22}
+print("name" in d)
+
+text = "hello"
+print('z' not in text)
+
+password = "admin@123"
+print('@' in password)
