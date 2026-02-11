42803d2c0c1e016cf0ac24cd0effa5dc83fac577
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-02-11 20:55:42 +0530

    Added new python program

---
+++ tuple.py
@@ -0,0 +1,16 @@
+#creating tuple
+numbers = (10, 20, 30, 40)
+print(numbers)
+
+#tuple indexing \
+numbers = (10, 20, 30, 40)
+
+print(numbers)
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[-1])
+
+#tuple slicing
+print(numbers[0:1])
+print(numbers[0:5])
