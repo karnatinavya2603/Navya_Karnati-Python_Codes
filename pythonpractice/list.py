a39f06e13344ca8d6d3449c4b749b365bda4530a
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-02-11 20:48:01 +0530

    Added new python program

---
+++ pythonpractice/list.py
@@ -0,0 +1,19 @@
+#create list 
+a = [10,20,30,40]
+print(a)
+
+#indexing
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[-1])
+
+#slicing 
+print(a[0:4])
+print(a[:3])
+print(a[:2])
+
+#step 
+print(a[0:3:2])
+print(a[0::1])
