65235895dbdb026d85a853807a368b74d0480908
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-02-11 20:14:16 +0530

    Updated python codes

---
+++ pythonpractice/max.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter number1:"))
+num2 = int(input("Enter number2:"))
+if num1 > num2:
+    print(f"{num1} is greater then {num2}")
+else:
+    print(f"{num1} is not greater then {num2}")
