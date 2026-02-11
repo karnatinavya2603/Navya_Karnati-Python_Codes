126c65b40a2bd255c3ef236750884d016faabc97
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-02-11 21:48:34 +0530

    Add programs to pythonpractice folder

---
+++ pythonpractice/dict.py
@@ -0,0 +1,18 @@
+student = {
+    "name":"Navya",
+    "Age": 23,
+    "city": "Hyderabad"
+}
+print(student)
+
+#Acess Values
+print(student["name"])
+print(student["Age"])
+
+#update 
+student["Age"] = 22
+print(student)
+
+#Add new 
+student["subject"] = "English"
+print(student)
---
+++ pythonpractice/set.py
@@ -0,0 +1,14 @@
+#create set
+set = {}
+print(set)
+#set 
+numbers = {10, 20, 30, 40,20}
+print(numbers)
+#Add element 
+numbers = {10, 20, 30}
+numbers.add(40)
+print(numbers)
+#remove
+numbers = {10, 20, 30}
+numbers.remove(30)
+print(numbers)
