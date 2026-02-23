d23da6e3c8b507867995e6058d4f24755c63988f
karnatinavya2603 <147272431+karnatinavya2603@users.noreply.github.com>
2026-02-23 14:11:10 +0530

    Added Python practice programs

--- files/f.py
+++ files/f.py
@@ -1,9 +1,46 @@
-file.txt='''Hi My name is Navya"
-"Welcomw to python'''
-def read_file(a):
-    data = open(a,"r")
-    b = data.read()
-    data.close()
-    return b
-print(read_file("file.txt")) 
+#file handling 
+f = open("Solutions.txt","r")
+str = f.read()
+print(str)
+f.close() 
+
+f = open("Solutions.txt","r")
+str = f.readline()
+print(str)
+f.close() 
+
+f = open("Solutions.txt","r")
+str = f.readlines()
+print(str)
+f.close()
+
+def file_read(a):
+    file =open(a,"r")
+    data = file.read()
+    file.close()
+    return data
+print(file_read("Solutions.txt"))
+
+def file_read(a):
+    file =open(a,"r")
+    data = file.readline()
+    file.close()
+    return data
+print(file_read("Solutions.txt"))
+
+def file_read(a):
+    file =open(a,"r")
+    data = file.readlines()
+    file.close()
+    return data
+print(file_read("Solutions.txt"))
+
+def file_read(a):
+    file = open(a,"r")
+    data = file.read()
+    for i in data:
+        print(i)
+    return i
+print(file_read("Solutions.txt"))
+
     
